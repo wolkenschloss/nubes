e2cf45715277065a947160374f04a164082fdbb1
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-05-31 16:36:50 +0200

    build: Run Configuration im Repository speichern (#30)

---
+++ services/blog/core/src/main/java/Dummy.java
@@ -0,0 +1,6 @@
+// Es muss mindestens eine Klasse in src/main/java enthalten sein,
+// weil ./gradlew quarkusDev sonst nicht funktioniert. Die Klasse
+// kann gelöscht werden, sobald andere Klassen in diesem Bibliothek
+// vorhanden sind.
+public class Dummy {
+}
