a723cefff96646702184dc4923730f210cbd1779
haschi <17554782+haschi@users.noreply.github.com>
2021-06-04 12:21:09 +0200

    feat(cookbook): Einfache Bootstrap Navigationsleiste (ohne Funktion)
    issue: #33

--- services/cookbook/webapp/src/App.vue
+++ services/cookbook/webapp/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container fluid="lg">
+      <b-navbar fixed="top" toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#">MyCloud</b-navbar-brand>
+        <b-navbar-toggle target="nav-collaps"></b-navbar-toggle>
+        <b-collapse id="nav-collaps" is-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+        </b-collapse>
+      </b-navbar>
+      <div id="main">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </b-container>
   </div>
 </template>
 
--- services/cookbook/webapp/src/main.js
+++ services/cookbook/webapp/src/main.js
@@ -3,8 +3,10 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import {IconsPlugin} from "bootstrap-vue";
 
 Vue.config.productionTip = false
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
