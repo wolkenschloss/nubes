905c8c892a5a7b828e35c5df3cb9384afce990ec
haschi <17554782+haschi@users.noreply.github.com>
2021-06-26 13:15:21 +0200

    bug(testbed): Stabile Voreinstellung für das Basisimage
    Es kann jederzeit das aktuelle Image heruntergeladen werden.
    Der Task Download verifiziert die Prüfsumme des Images und
    die Signatur der Prüfsummendatei.
    
    issue: #77

--- buildSrc/src/main/java/wolkenschloss/DownloadTask.java
+++ buildSrc/src/main/java/wolkenschloss/DownloadTask.java
@@ -1,14 +1,13 @@
 package wolkenschloss;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
 import org.gradle.api.GradleScriptException;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileSystemOperations;
+import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.CacheableTask;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.OutputDirectory;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.*;
 import org.gradle.internal.logging.progress.ProgressLoggerFactory;
 import org.gradle.process.ExecOperations;
 
@@ -51,8 +50,11 @@ abstract public class DownloadTask extends DefaultTask {
         return parent.resolve("SHA256SUMS.gpg").toURL();
     }
 
-    @OutputDirectory
-    abstract public DirectoryProperty getDownloads();
+    @Internal
+    abstract protected DirectoryProperty getDownloads();
+
+    @OutputFile
+    abstract public RegularFileProperty getBaseImage();
 
     @Inject
     protected abstract ProgressLoggerFactory getProgressLoggerFactory();
@@ -60,40 +62,48 @@ abstract public class DownloadTask extends DefaultTask {
     @TaskAction
     public void download() throws URISyntaxException, IOException {
 
-            downloadFile(getBaseImageUrl());
-            downloadFile(getSha256SumsUrl());
-            downloadFile(getGpgFileUrl());
+        downloadFile(getBaseImageUrl());
+        downloadFile(getSha256SumsUrl());
+        downloadFile(getGpgFileUrl());
 
-            verifySignature();
-            verifyChecksum();
+        verifySignature();
+        verifyChecksum();
 
-//        var file = new File(getBaseImage().getAsFile().get().toPath().toString());
-//
-//        var success = file.setWritable(false, false)
-//                && file.setReadable(true, true)
-//                && file.setExecutable(false, false);
-//        if (!success)  {
-//            throw new GradleException("Die Dateiberechtigungen konnten nicht geändert werden.");
-//        }
+        changePermissions();
     }
 
-    private void verifyChecksum() throws IOException {
-        var result = getExecOperations().exec(spec -> spec.commandLine("sha256sum")
+    private void changePermissions() {
+
+        var file = new File(getBaseImage().getAsFile().get().toPath().toString());
+
+        var success = file.setWritable(false, false)
+                && file.setReadable(true, true)
+                && file.setExecutable(false, false);
+        if (!success)  {
+            throw new GradleException("Die Dateiberechtigungen konnten nicht geändert werden.");
+        }
+    }
+
+    private Path downloadPath(String filename) {
+        return getDownloads().get().file(filename).getAsFile().toPath();
+    }
+
+    private void verifyChecksum() {
+        getExecOperations().exec(spec -> spec.commandLine("sha256sum")
                 .args("--ignore-missing",
                         "--check",
-                        getDownloads().get().file("SHA256SUMS").getAsFile().toPath())
-                .workingDir(getDownloads().get()));
-        result.assertNormalExitValue();
+                        downloadPath("SHA256SUMS"))
+                .workingDir(getDownloads().get()))
+                .assertNormalExitValue();
     }
 
 
     private void verifySignature() {
-        getExecOperations().exec(spec -> {
-            spec.commandLine("gpg")
-                    .args("--keyid-format", "long", "--verify",
-                            getDownloads().get().file("SHA256SUMS.gpg").getAsFile().toPath(),
-                            getDownloads().get().file("SHA256SUMS").getAsFile().toPath());
-        }).assertNormalExitValue();
+        getExecOperations().exec(spec -> spec.commandLine("gpg")
+                .args("--keyid-format", "long", "--verify",
+                        downloadPath("SHA256SUMS.gpg"),
+                        downloadPath("SHA256SUMS")))
+                .assertNormalExitValue();
     }
 
     private void downloadFile(URL src) throws IOException {
@@ -101,28 +111,30 @@ abstract public class DownloadTask extends DefaultTask {
 
         progressLogger.start("Download base Image", src.getFile());
 
-        InputStream input = src.openStream();
-        OutputStream output;
+        try (InputStream input = src.openStream()) {
 
-        var filename = Path.of(src.getFile()).getFileName();
-        var dst = getDownloads().get().file(filename.toString());
-        try {
-            output = new FileOutputStream(dst.getAsFile());
-        } catch (FileNotFoundException e) {
-            throw new GradleScriptException("Datei nicht geschrieben werden", e);
+            OutputStream output;
+            var filename = Path.of(src.getFile()).getFileName();
+            var dst = getDownloads().get().file(filename.toString());
+
+            try {
+                output = new FileOutputStream(dst.getAsFile());
+            } catch (FileNotFoundException e) {
+                throw new GradleScriptException("Datei nicht geschrieben werden", e);
+            }
+
+            byte[] buffer = new byte[1444];
+            int byteRead;
+            int byteSum = 0;
+            while ((byteRead = input.read(buffer)) != -1) {
+                byteSum += byteRead;
+                output.write(buffer, 0, byteRead);
+                progressLogger.progress(String.format("%d MB", byteSum / (1024 * 1024)));
+            }
+
+            output.close();
         }
 
-        byte[] buffer = new byte[1444];
-        int byteRead;
-        int byteSum = 0;
-        while ((byteRead = input.read(buffer)) != -1) {
-            byteSum += byteRead;
-            output.write(buffer, 0, byteRead);
-            progressLogger.progress(String.format("%d MB", byteSum / (1024 * 1024)));
-        }
-
-        input.close();
-        output.close();
         progressLogger.completed();
     }
 }
