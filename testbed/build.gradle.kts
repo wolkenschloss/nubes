7e2d3113a001f74764e88864b4e7f537ad898411
haschi <17554782+haschi@users.noreply.github.com>
2021-07-03 19:06:26 +0200

    build: replace groovy build scripts
    from ./testbed/build.gradle to ./testbed/build.gradle.kts

---
+++ testbed/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("wolkenschloss.testbed")
+}
+
+defaultTasks "start"
+
+testbed {
+    base {
+        name.set("ubuntu-20.04")
+        url.set("https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-disk-kvm.img")
+    }
+
+    domain {
+        name.set("testbed")
+    }
+    pool {
+        name.set("testbed")
+    }
+}
