54fce0005e0f3e31804f89d02b8d4cf4e1c281ac
haschi <17554782+haschi@users.noreply.github.com>
2021-07-03 18:19:07 +0200

    build: replace groovy build scripts
    from ./build.gradle to ./build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("io.quarkus:io.quarkus.gradle.plugin:1.13.7.Final")
+    }
+}
+
+allprojects {
+    group = "family.haschka.mycloud"
+    version = "0.0.1-SNAPSHOT"
+}
