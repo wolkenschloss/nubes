e713c1d517622d82aa1a49f21f4b0feffebf140e
haschi <17554782+haschi@users.noreply.github.com>
2021-07-13 06:42:52 +0200

    refactor(wolkenschloss-conventions): reorder
    newspaper style

--- buildSrc/src/main/java/wolkenschloss/domain/DomainTasks.java
+++ buildSrc/src/main/java/wolkenschloss/domain/DomainTasks.java
@@ -23,21 +23,36 @@ public class DomainTasks {
         this.port = port;
     }
 
-    private static void registerStartTask(TaskContainer tasks) {
-        tasks.register(
-                START_TASK_NAME,
-                DefaultTask.class,
-                task -> {
-                    task.setGroup(GROUP_NAME);
-                    task.setDescription("The all in one lifecycle start task. Have a cup of coffee.");
-                    task.dependsOn(tasks.named(READ_KUBE_CONFIG_TASK_NAME));
-                });
-    }
-
     public void register(TaskContainer tasks) {
         registerBuildDomainTask(tasks);
         registerReadKubeConfig(tasks);
         registerStartTask(tasks);
+    }
+
+    private void registerBuildDomainTask(TaskContainer tasks) {
+
+        var buildPool = tasks.findByName(PoolTasks.BUILD_POOL_TASK_NAME);
+
+        var domainDescription = tasks
+                .named(
+                        TransformationTasks.TRANSFORM_DOMAIN_DESCRIPTION_TASK_NAME,
+                        Transform.class)
+                .map(Transform::getOutputFile)
+                .get();
+
+        tasks.register(
+                BUILD_DOMAIN_TASK_NAME,
+                BuildDomain.class,
+                task -> {
+                    task.setGroup(GROUP_NAME);
+                    task.dependsOn(buildPool);
+                    task.setDescription("Starts the libvirt domain and waits for the callback.");
+                    task.getDomain().convention(domain.getName());
+                    task.getPort().convention(port);
+                    task.getXmlDescription().convention(domainDescription);
+                    task.getKnownHostsFile().convention(domain.getKnownHostsFile());
+                    task.getDomainOperations().set(domain.getDomainOperations());
+                });
     }
 
     private void registerReadKubeConfig(TaskContainer tasks) {
@@ -58,29 +73,14 @@ public class DomainTasks {
                 });
     }
 
-    private void registerBuildDomainTask(TaskContainer tasks) {
-
-        var buildPool = tasks.findByName(PoolTasks.BUILD_POOL_TASK_NAME);
-
-        var domainDescription = tasks
-                .named(
-                    TransformationTasks.TRANSFORM_DOMAIN_DESCRIPTION_TASK_NAME,
-                    Transform.class)
-                .map(Transform::getOutputFile)
-                .get();
-
+    private static void registerStartTask(TaskContainer tasks) {
         tasks.register(
-                BUILD_DOMAIN_TASK_NAME,
-                BuildDomain.class,
+                START_TASK_NAME,
+                DefaultTask.class,
                 task -> {
                     task.setGroup(GROUP_NAME);
-                    task.dependsOn(buildPool);
-                    task.setDescription("Starts the libvirt domain and waits for the callback.");
-                    task.getDomain().convention(domain.getName());
-                    task.getPort().convention(port);
-                    task.getXmlDescription().convention(domainDescription);
-                    task.getKnownHostsFile().convention(domain.getKnownHostsFile());
-                    task.getDomainOperations().set(domain.getDomainOperations());
+                    task.setDescription("The all in one lifecycle start task. Have a cup of coffee.");
+                    task.dependsOn(tasks.named(READ_KUBE_CONFIG_TASK_NAME));
                 });
     }
 }
