4dba2ca22685006c2a728a3280be903ff1df4f5c
haschi <17554782+haschi@users.noreply.github.com>
2021-07-13 03:15:51 +0200

    refactor(wolkenschloss-conventions): change implementation
    add group and description

--- buildSrc/src/main/java/wolkenschloss/download/DownloadTasksRegistrar.java
+++ buildSrc/src/main/java/wolkenschloss/download/DownloadTasksRegistrar.java
@@ -4,6 +4,7 @@ import org.gradle.api.tasks.TaskContainer;
 
 public class DownloadTasksRegistrar {
     public static final String DOWNLOAD_DISTRIBUTION_TASK_NAME = "download";
+    public static final String GROUP_NAME = "download";
 
     private final TaskContainer tasks;
 
@@ -16,6 +17,8 @@ public class DownloadTasksRegistrar {
                 DOWNLOAD_DISTRIBUTION_TASK_NAME,
                 DownloadDistribution.class,
                 task -> {
+                    task.setGroup(GROUP_NAME);
+                    task.setDescription("Downloads the base image to a cache for later use.");
                     task.getBaseImageLocation().convention(baseImage.getUrl());
                     task.getDistributionName().convention(baseImage.getName());
                     task.getBaseImage().convention(baseImage.getBaseImageFile());
