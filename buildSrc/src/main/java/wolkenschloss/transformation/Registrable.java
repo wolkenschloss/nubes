8a6dd81761b2f267758e365d3439fdf906e17707
haschi <17554782+haschi@users.noreply.github.com>
2021-07-13 03:25:00 +0200

    refactor(wolkenschloss-conventions): optimize imports

--- buildSrc/src/main/java/wolkenschloss/transformation/Registrable.java
+++ buildSrc/src/main/java/wolkenschloss/transformation/Registrable.java
@@ -1,7 +1,6 @@
 package wolkenschloss.transformation;
 
 import org.gradle.api.tasks.TaskContainer;
-import org.gradle.api.tasks.TaskProvider;
 
 public interface Registrable {
     void register(TaskContainer tasks);
