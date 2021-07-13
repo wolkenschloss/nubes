84cee539f902a4df4ee2916ee35bd22cb4f3ee01
haschi <17554782+haschi@users.noreply.github.com>
2021-07-13 03:24:02 +0200

    refactor(wolkenschloss-conventions): optimize imports

--- buildSrc/src/main/java/wolkenschloss/transformation/TaskBuilder.java
+++ buildSrc/src/main/java/wolkenschloss/transformation/TaskBuilder.java
@@ -1,11 +1,9 @@
 package wolkenschloss.transformation;
 
-import org.gradle.api.Project;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.RegularFile;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.TaskContainer;
-import org.gradle.api.tasks.TaskProvider;
 
 import java.util.function.Function;
 
