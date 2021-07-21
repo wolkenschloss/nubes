70cd92e2c506465d00b182bec404f1471cdb33e9
haschi <17554782+haschi@users.noreply.github.com>
2021-07-21 02:17:36 +0200

    feat: Gradle Build Cache für vue, unit und e2e Tasks
    Tests für wolkenschloss.conventions.webapp Plugin:
    Prüfen, ob Testreports erstellt wurden.
    Testreports sind Build-Ergebnisse der Test Tasks.
    
    issue: #118

--- services/cookbook/webapp/jest.config.js
+++ services/cookbook/webapp/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
   setupFilesAfterEnv: ["./tests/unit/jest.setup.js"],
   reporters: ["default", ['jest-junit', {
-    outputDirectory: 'build/results/tests/unit',
+    outputDirectory: 'build/reports/tests/unit',
     classNameTemplate: '{classname}',
     titleTemplate: '{title}'
   }]],
