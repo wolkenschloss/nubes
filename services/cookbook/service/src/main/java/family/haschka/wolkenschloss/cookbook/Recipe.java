c712278f8aa29879590a2e22854d1f7db931b758
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-07-24 20:48:10 +0200

    feat: Update auf Quarkus 2 (#122)
    Notwendige Anpassungen:
    Konfiguration des ObjectMappers, so dass unbekannte Felder
    zu einem Fehler bei der Deserialisierung führen.
    
    Eigenschaft resteasy.patchfilter.legacy=true erforderlich,
    damit PATCH für Jackson wie gewohnt ausgeführt werden kann.
    
    issue: #91

--- services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/Recipe.java
+++ services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/Recipe.java
@@ -1,12 +1,10 @@
 package family.haschka.wolkenschloss.cookbook;
 
-import io.quarkus.mongodb.panache.MongoEntity;
 import org.bson.codecs.pojo.annotations.BsonId;
 
 import java.util.List;
 import java.util.UUID;
 
-@MongoEntity
 public class Recipe {
     @BsonId
     public UUID recipeId;
