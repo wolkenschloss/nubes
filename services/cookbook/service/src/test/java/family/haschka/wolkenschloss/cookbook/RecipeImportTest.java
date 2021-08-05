e60fb0c6fef58eb43ae957c43fb2a10c7f66956b
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-08-05 11:08:27 +0200

    feat(cookbook): import recipe

--- services/cookbook/service/src/test/java/family/haschka/wolkenschloss/cookbook/RecipeImportTest.java
+++ services/cookbook/service/src/test/java/family/haschka/wolkenschloss/cookbook/RecipeImportTest.java
@@ -1,18 +1,33 @@
 package family.haschka.wolkenschloss.cookbook;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.List;
 
 public class RecipeImportTest {
 
-    @Test
-    public void xxx() {
-        var reader = new ResourceHtmlReader("recipe.html");
+    public enum Testcase {
+        LASAGNE("recipe.html", "Lasagne");
+
+        private final String filename;
+        private final String name;
+
+        Testcase(String filename, String name) {
+            this.filename = filename;
+            this.name = name;
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    public void xxx(Testcase testcase) {
+        var reader = new ResourceHtmlReader(testcase.filename);
         var importer = new RecipeImport(reader);
         List<Recipe> recipes = importer.extract();
 
         Assertions.assertEquals(recipes.size(), 1);
+        Assertions.assertEquals(recipes.get(0).title, testcase.name);
     }
 }
