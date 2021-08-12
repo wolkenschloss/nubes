2c662c6c5a418ce2d9e30e0158adc4b1b62fecb2
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-08-12 17:44:21 +0200

    nubes-143 Wechsel von Java 11 auf Java 16
    JobReceivedEvent als Java Record

--- services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/job/JobReceivedEvent.java
+++ services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/job/JobReceivedEvent.java
@@ -4,28 +4,3 @@ import java.net.URI;
 import java.util.UUID;
 
 public record JobReceivedEvent(UUID jobId, URI source){}
-//public class JobReceivedEvent {
-//    public UUID jobId;
-//    public URI source;
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        JobReceivedEvent that = (JobReceivedEvent) o;
-//        return Objects.equals(jobId, that.jobId) && Objects.equals(source, that.source);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(jobId, source);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "JobReceivedEvent{" +
-//                "jobId=" + jobId +
-//                ", source=" + source +
-//                '}';
-//    }
-//}
