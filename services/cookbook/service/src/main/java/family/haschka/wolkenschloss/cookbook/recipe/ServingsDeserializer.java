55b512033bc5695e698a60b5908f1b422a5bedcf
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-08-26 15:16:55 +0200

    bug(cookbook): can not import recipe
    issue: #157

--- services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/recipe/ServingsDeserializer.java
+++ services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/recipe/ServingsDeserializer.java
@@ -4,11 +4,19 @@ import javax.json.bind.serializer.DeserializationContext;
 import javax.json.bind.serializer.JsonbDeserializer;
 import javax.json.stream.JsonParser;
 import java.lang.reflect.Type;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ServingsDeserializer implements JsonbDeserializer<Servings> {
     @Override
     public Servings deserialize(JsonParser parser, DeserializationContext ctx, Type rtType) {
-        Integer servings = ctx.deserialize(Integer.class, parser);
-        return new Servings(servings);
+        var input = ctx.deserialize(String.class, parser);
+        Pattern p = Pattern.compile("(?<servings>[1-9][0-9]*)");
+        Matcher m = p.matcher(input);
+        if (m.find()) {
+            String servings = m.group("servings");
+            return new Servings(Integer.parseInt(servings));
+        } else
+            return new Servings(1);
     }
 }
