8aa2a8f544900443fc9e4ef0f9a5769d9786c3f7
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-10-18 11:55:15 +0200

    nubes-164 Weitere Einheiten für Zutaten (#169)

---
+++ services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/recipe/Unit.java
@@ -0,0 +1,61 @@
+package family.haschka.wolkenschloss.cookbook.recipe;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+// https://www.kochwiki.org/wiki/Zubereitung:Ma%C3%9Fe_und_Gewichte
+public enum Unit {
+    // Volumenangaben
+    CUBIC("cubic", "Kubikzentimeter", "cc", "centimeter", "ccm"),
+    LITER("Liter", "l"),
+    DECILITER("decilitre", "Deziliter", "dl"),
+    CENTILITER("centilitre", "Centiliter", "cl"),
+    MILLILITER("millilitre", "Milliliter", "ml"),
+    FLUID_ONCE("fluid ounce", "fl", "fl[\\. ]oz\\.?", "oz[\\. ]fl\\.?"),
+    CUP("cup", "c", "Tasse", "Ta"),
+
+    // Gewichtsangaben
+    GRAM("gram", "g", "Gramm"),
+    DECIGRAM("decigram", "dg", "Dezigramm"),
+    MILLIGRAM("milligram", "mg", "Milligramm"),
+    DEKAGRAM("Dekagramm", "dag", "Deka"),
+    KILOGRAM("kilogram", "kg", "Kilogramm"),
+    POUND("pound", "lb", "Pfund"),
+
+    // Küchenbegriffe
+    BUNCH("bunch", "bu", "Bund", "Bd"),
+    DROP("drop", "dr", "Tropfen", "Tr"),
+    DASH("dash", "ds", "Spritzer", "Spr"),
+    SHOT("shot", "Schuss"),
+    TEASPOON("teaspoon", "ts", "tsp", "Teelöffel", "TL", "Barlöffel", "BL"),
+    TABLESPOON("tablespoon", "tb", "tbsp", "Esslöffel", "EL"),
+    MESSERSPITZE("Messerspitze", "Msp"),
+    PINCH("pinch", "pn", "Prise", "Pr"),
+
+    // Allgemeine Begriffe. Es fehlen noch Modifikationen klein, mittelgroß, groß
+    CAN("can", "cn", "Dose", "Do"),
+    CARTON("carton", "ct", "Karton", "Kt"),
+    PIECE("piece", "pc", "Stück", "Stk"),
+    PACKAGE("package", "pk", "Packung", "Pk", "Päckchen", "Pck"),
+    SLICE("slice", "sl", "Scheibe", "Sch"),
+    SHEET("sheet", "sh", "Blatt", "Bl")
+    ;
+
+
+    private final String unit;
+    private final String[] aliases;
+
+    Unit(String unit, String... aliases) {
+        this.unit = unit;
+        this.aliases = aliases;
+    }
+
+    public String pattern() {
+        return unit  + "|" + String.join("|", Arrays.asList(aliases));
+    }
+
+    public static String regex() {
+        return Arrays.stream(Unit.values()).map(v -> v.pattern())
+                .collect(Collectors.joining("|"));
+    }
+}
---
+++ services/cookbook/service/src/test/java/family/haschka/wolkenschloss/cookbook/recipe/UnitTest.java
@@ -0,0 +1,66 @@
+package family.haschka.wolkenschloss.cookbook.recipe;
+
+import com.mongodb.assertions.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UnitTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "cubic", "Liter", "decilitre", "Deziliter", "dl",
+            "centilitre", "Centiliter", "cl",
+            "millilitre", "Milliliter", "ml",
+            "fluid ounce", "fl", "fl.oz.", "oz.fl.", "fl oz", "oz fl",
+            "cup", "c", "Tasse", "Ta",
+            "gram", "g", "Gramm",
+            "decigram", "dg", "Dezigramm",
+            "milligram", "mg", "Milligramm",
+            "Dekagramm", "dag", "Deka",
+            "kilogram", "kg", "Kilogramm",
+            "pound", "lb", "Pfund",
+
+            "bunch", "bu", "Bund", "Bd",
+            "drop", "dr", "Tropfen", "Tr",
+            "dash", "ds", "Spritzer", "Spr",
+            "shot", "Schuss",
+            "teaspoon", "ts", "tsp", "Teelöffel", "TL",
+            "Barlöffel", "BL",
+            "tablespoon", "tb", "tbsp", "Esslöffel", "EL",
+            "Messerspitze", "Msp",
+            "pinch", "pn", "Prise", "Pr",
+
+            "can", "cn", "Dose", "Do",
+            "carton", "ct", "Karton", "Kt",
+            "piece", "pc", "Stück", "Stk",
+            "package", "pk", "Packung", "Pk", "Päckchen", "Pck",
+            "slice", "sl", "Scheibe", "Sch",
+            "sheet", "sh", "Blatt", "Bl"
+    })
+    public void shouldCreateRegex(String testcase) {
+        Pattern regex = Pattern.compile("^" + Unit.regex() + "$");
+        Matcher matcher = regex.matcher(testcase);
+
+        Assertions.assertTrue(matcher.matches());
+    }
+
+    enum UnitTestcase {
+        CUBIC("cubic"),
+        LITER("Liter"),
+        DECILITER("decilitre", "Deziliter", "dl"),
+        CENTILITER("centilitre", "Centiliter", "cl"),
+        MILLILITER("millilitre", "Milliliter", "ml"),
+        FLUID_ONCE("fluid ounce", "fl", "fl.oz.", "oz.fl."),
+        CUP("cup", "c", "Tasse", "Ta");
+
+        private final String unit;
+
+        UnitTestcase(String unit, String... aliases) {
+            this.unit = unit;
+        }
+    }
+}
