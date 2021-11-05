69911586eb29538b9aa635ebecd2fe5cb711738f
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-11-05 17:34:09 +0100

    nubes-47 Container Build für Backend Services
    HOST Variable wird nicht mehr verwendet.
    
    issue: #47

--- services/cookbook/service/src/integrationTest/java/familie/haschka/wolkenschloss/cookbook/testing/WiremockRecipes.java
+++ services/cookbook/service/src/integrationTest/java/familie/haschka/wolkenschloss/cookbook/testing/WiremockRecipes.java
@@ -3,12 +3,14 @@ package familie.haschka.wolkenschloss.cookbook.testing;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import org.eclipse.microprofile.config.ConfigProvider;
 
 import java.util.Collections;
 import java.util.Map;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
 public class WiremockRecipes implements QuarkusTestResourceLifecycleManager {
 
@@ -16,16 +18,19 @@ public class WiremockRecipes implements QuarkusTestResourceLifecycleManager {
 
     @Override
     public Map<String, String> start() {
+
+        System.out.println("Starting Wiremock Server");
         wireMockServer = new WireMockServer(
                 WireMockConfiguration.options()
-                    .fileSource(new ClasspathFileSourceWithoutLeadingSlash())
+                        .fileSource(new ClasspathFileSourceWithoutLeadingSlash())
+
         );
 
         wireMockServer.start();
 
         stubFor(get(urlEqualTo("/lasagne.html"))
                 .willReturn(aResponse()
-                        .withFixedDelay(3000)
+                        .withFixedDelay(500)
                         .withStatus(200)
                         .withHeader("Content-Type", "text/html")
                         .withHeader("X-Files-Root", wireMockServer.getOptions().filesRoot().getPath())
@@ -43,15 +48,18 @@ public class WiremockRecipes implements QuarkusTestResourceLifecycleManager {
                         .withHeader("Content-Type", "text/html")
                         .withBodyFile("menu.html")));
 
-        var host = ConfigProvider.getConfig().getValue("wolkenschloss.nubes.testhost", String.class);
-
-        return Collections.singletonMap("family.haschka.wiremock.recipes",
-                wireMockServer.baseUrl().replace("localhost", host));
+        try {
+            return Collections.singletonMap(
+                    "family.haschka.wiremock.recipes",
+                    wireMockServer.baseUrl().replace("localhost", IpUtil.getHostAddress()));
+        } catch (UnknownHostAddress e) {
+            return Collections.emptyMap();
+        }
     }
 
     @Override
     public void stop() {
-        if(wireMockServer != null) {
+        if (wireMockServer != null) {
             wireMockServer.stop();
         }
     }
