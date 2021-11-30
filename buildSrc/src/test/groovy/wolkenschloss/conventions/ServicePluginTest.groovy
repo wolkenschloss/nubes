a5d609f1d6d87eef0742990a9487c4d9d7cdf6aa
54v054r3n <17554782+haschi@users.noreply.github.com>
2021-11-30 14:23:58 +0100

    test: service plugin compilation test

---
+++ buildSrc/src/test/groovy/wolkenschloss/conventions/ServicePluginTest.groovy
@@ -0,0 +1,108 @@
+package wolkenschloss.conventions
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.Specification
+import spock.lang.TempDir
+
+class ServicePluginTest extends Specification {
+    @TempDir File testProjectDir
+    File settings
+    File build
+    File test
+    File source
+    File properties
+
+    def "should compile quarkus service"() {
+        settings = new File(testProjectDir, "settings.gradle")
+        build = new File(testProjectDir, "build.gradle")
+
+        properties = new File(testProjectDir, "gradle.properties")
+
+        settings << """
+            rootProject.name = "service-plugin-test"
+        """
+
+        properties << """
+            quarkusPlatformGroupId=io.quarkus
+            quarkusPlatformArtifactId=quarkus-bom
+            quarkusPlatformVersion=2.5.0.Final
+            version = 999-SNAPSHOT
+            group = family.haschka.wolkenschloss.conventions
+            vcs.commit = unspecified
+            vcs.ref = unspecified 
+        """
+
+        build << """
+            plugins {
+                id 'wolkenschloss.conventions.service'
+            }
+            
+            dependencies {
+                implementation("io.quarkus:quarkus-resteasy")
+            }
+        """
+
+        source = new File(testProjectDir, "src/main/java/GreetingResource.java")
+        source.parentFile.mkdirs()
+        source << """
+            package family.haschka.wolkenschloss.convention.service;
+            
+            import javax.inject.Inject;
+            import javax.ws.rs.GET;
+            import javax.ws.rs.Path;
+            import javax.ws.rs.core.MediaType;
+            import javax.ws.rs.Produces;
+            
+            @Path("/greeting")
+            public class GreetingResource {
+                                   
+                @GET
+                @Produces(MediaType.TEXT_PLAIN)
+                public String greet() {
+                    return "hello";
+                }            
+            }
+        """
+
+        test = new File(testProjectDir, "src/test/java/GreetingResourceTest.java")
+        test.parentFile.mkdirs()
+        test << """
+        package family.haschka.wolkenschloss.convention.service;
+        
+        import io.quarkus.test.common.http.TestHTTPEndpoint;
+        import io.quarkus.test.common.http.TestHTTPResource;
+        import io.quarkus.test.junit.QuarkusTest;
+        import io.restassured.RestAssured;
+        import org.junit.jupiter.api.Test;
+        import javax.ws.rs.core.Response;
+        import java.net.URL;
+        
+        @QuarkusTest
+        public class GreetingResourceTest {
+            @TestHTTPEndpoint(GreetingResource.class)
+            @TestHTTPResource
+            URL url;
+            
+            @Test
+            public void getGreetingTest() {
+                RestAssured.given()
+                .when()
+                .get(url)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode()); 
+            }
+        }
+        """
+
+        when: "running gradle build"
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir)
+            .withArguments("build")
+            .withPluginClasspath()
+            .build()
+
+        then:
+        result.task(":build").outcome == TaskOutcome.SUCCESS
+    }
+}
