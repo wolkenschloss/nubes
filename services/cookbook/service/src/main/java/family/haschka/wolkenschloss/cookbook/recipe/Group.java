a82a9cfce530cca92f9761d0da4751d77287f4cd
54v054r3n <17554782+haschi@users.noreply.github.com>
2022-01-05 15:44:40 +0100

    feat: Obtain units for entering ingredients from the server
    introduce group resource

--- services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/recipe/Group.java
+++ services/cookbook/service/src/main/java/family/haschka/wolkenschloss/cookbook/recipe/Group.java
@@ -2,9 +2,9 @@ package family.haschka.wolkenschloss.cookbook.recipe;
 
 public enum Group {
 
-    VOLUME("Volumes", "", Unit.CUBIC),
-    WEIGHT("Weights", "", Unit.GRAM),
-    KITCHEN("Kitchen Terms", "", Unit.BUNCH),
+    VOLUME("Volumes", "", Unit.CUBIC, Unit.LITER, Unit.DECILITER, Unit.CENTILITER, Unit.MILLILITER, Unit.FLUID_ONCE, Unit.CUP),
+    WEIGHT("Weights", "", Unit.GRAM, Unit.DECIGRAM, Unit.MILLIGRAM, Unit.DEKAGRAM, Unit.KILOGRAM, Unit.POUND),
+    KITCHEN("Kitchen Terms", "", Unit.BUNCH, Unit.DROP, Unit.DASH, Unit.SHOT, Unit.TEASPOON, Unit.TABLESPOON, Unit.MESSERSPITZE, Unit.PINCH),
     COMMON("Common Terms", "", Unit.CAN);
 
     final String description;
