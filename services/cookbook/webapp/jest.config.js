2cae51c54da81fd8d2a5192554255ffde5273f8c
54v054r3n <17554782+haschi@users.noreply.github.com>
2022-01-07 17:37:37 +0100

    build(cookbook): Write a jest reporter who works well with gradle (#263)
    Closes #255

--- services/cookbook/webapp/jest.config.js
+++ services/cookbook/webapp/jest.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!vuetify)'
-  ],
-  setupFilesAfterEnv: ["./tests/unit/jest.setup.js"],
-  reporters: ["default", ['jest-junit', {
-    outputDirectory: 'build/reports/tests/unit',
-    classNameTemplate: '{classname}',
-    titleTemplate: '{title}'
-  }]],
+    preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+    transformIgnorePatterns: [
+        '<rootDir>/node_modules/(?!vuetify)'
+    ],
+    setupFilesAfterEnv: ["./tests/unit/jest.setup.js"],
+    reporters: [
+        'jest-standard-reporter',
+        ['jest-junit', {
+            outputDirectory: 'build/reports/tests/unit',
+            classNameTemplate: '{classname}',
+            titleTemplate: '{title}'
+        }]],
 }
