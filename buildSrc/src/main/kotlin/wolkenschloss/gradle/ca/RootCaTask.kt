2fe2e6c5e15521d2d0ddcc9c21794c2f2104c513
54v054r3n <17554782+haschi@users.noreply.github.com>
2022-02-02 16:11:05 +0100

    build: ca plugin skeleton

--- buildSrc/src/main/kotlin/wolkenschloss/gradle/ca/RootCaTask.kt
+++ buildSrc/src/main/kotlin/wolkenschloss/gradle/ca/RootCaTask.kt
@@ -4,6 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import java.security.KeyPairGenerator
 
 abstract class RootCaTask : DefaultTask() {
 
@@ -13,5 +14,15 @@ abstract class RootCaTask : DefaultTask() {
     @TaskAction
     fun execute() {
         outputDir.get().asFile.mkdirs()
+        val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+        val keyPair = keyPairGenerator.genKeyPair()
+
+        val caPrivateKeyFile = outputDir.file("ca.key").get().asFile
+        caPrivateKeyFile.createNewFile()
+        caPrivateKeyFile.writeBytes(keyPair.private.encoded)
+
+        val caPublicKeyFile = outputDir.file("ca.pub").get().asFile
+        caPublicKeyFile.createNewFile()
+        caPublicKeyFile.writeBytes(keyPair.public.encoded)
     }
 }
