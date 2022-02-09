7e9f763cacb5c3217bbeedd6f2ac476d115a8191
54v054r3n <17554782+haschi@users.noreply.github.com>
2022-02-09 15:47:23 +0100

    refactor: Fixtures track their clones

--- buildSrc/src/test/functional/kotlin/wolkenschloss/conventions/WebappTest.kt
+++ buildSrc/src/test/functional/kotlin/wolkenschloss/conventions/WebappTest.kt
@@ -1,7 +1,6 @@
 package wolkenschloss.conventions
 
 import io.kotest.assertions.assertSoftly
-import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.file.shouldBeADirectory
@@ -11,85 +10,72 @@ import io.kotest.matchers.shouldBe
 import org.gradle.testkit.runner.TaskOutcome
 import wolkenschloss.testing.Fixtures
 import wolkenschloss.testing.build
-import java.nio.file.Paths
 
 class WebappTest : DescribeSpec({
 
     describe("project with vue application") {
-        // It is not possible to use the temporary directories provided by
-        // Kotest or Java for cloned fixtures.
-        //
-        // The node_modules directory contains executable files that are
-        // required for the test. No files may be executed in proper temporary
-        // directories. This leads to an error in the test.
-        val fixture = Paths.get(System.getProperty("user.dir"), "build", "tmp", "fixture").toFile()
 
-        beforeEach {
-            Fixtures("webapp").clone(fixture)
-            fixture.shouldExist()
-        }
-
-        afterEach {
-            fixture.walkBottomUp().forEach {
-                withClue("deleting ${it.path}") {
-                    it.delete() shouldBe true
-                }
-            }
-        }
+        val fixture = autoClose(Fixtures("webapp"))
 
         describe("build task") {
             it("should create jar file") {
+                fixture.withClone {
+                    val result = build("build")
 
-                val result = fixture.build("build")
-
-                result.task(":build")!!.outcome shouldBe TaskOutcome.SUCCESS
-                result.task(":vue")!!.outcome shouldBe TaskOutcome.SUCCESS
-                fixture.resolve("build/libs/fixture-webapp.jar").shouldExist()
-
+                    result.task(":build")!!.outcome shouldBe TaskOutcome.SUCCESS
+                    result.task(":vue")!!.outcome shouldBe TaskOutcome.SUCCESS
+                    resolve("build/libs/fixture-webapp.jar").shouldExist()
+                }
             }
         }
 
         describe("check task") {
             it("should run unit and e2e tasks") {
-                val result = fixture.build("check")
+                fixture.withClone {
+                    val result = build("check")
 
-                result.tasks(TaskOutcome.SUCCESS)
-                    .map { task -> task.path }
-                    .shouldContainAll(":unit", ":e2e", ":check")
+                    result.tasks(TaskOutcome.SUCCESS)
+                        .map { task -> task.path }
+                        .shouldContainAll(":unit", ":e2e", ":check")
+                }
             }
         }
 
         describe("vue task") {
             it("should build vue app") {
-                val result = fixture.build("vue")
+                fixture.withClone {
+                    val result = build("vue")
 
-                result.task(":vue")!!.outcome shouldBe TaskOutcome.SUCCESS
+                    result.task(":vue")!!.outcome shouldBe TaskOutcome.SUCCESS
 
-                assertSoftly(fixture.resolve("build/classes/java/main/META-INF/resources")) {
-                    shouldBeADirectory()
-                    shouldContainFile("index.html")
-                    resolve("js").shouldBeADirectory()
+                    assertSoftly(resolve("build/classes/java/main/META-INF/resources")) {
+                        shouldBeADirectory()
+                        shouldContainFile("index.html")
+                        resolve("js").shouldBeADirectory()
+                    }
                 }
             }
         }
 
         describe("unit task") {
             it("should run unit tests") {
-                val result = fixture.build("unit")
+                fixture.withClone {
+                    val result = build("unit")
 
-                result.task(":unit")!!.outcome shouldBe TaskOutcome.SUCCESS
-                fixture.resolve("build/reports/tests/unit")
-                    .shouldContainFile("junit.xml")
+                    result.task(":unit")!!.outcome shouldBe TaskOutcome.SUCCESS
+                    resolve("build/reports/tests/unit").shouldContainFile("junit.xml")
+                }
             }
         }
 
         describe("e2e task") {
             it("should run e2e tests") {
-                val result = fixture.build("e2e")
+                fixture.withClone {
+                    val result = build("e2e")
 
-                result.task(":e2e")!!.outcome shouldBe TaskOutcome.SUCCESS
-                val reports = fixture.resolve("build/reports/tests/e2e")
-                reports.shouldBeADirectory()
+                    result.task(":e2e")!!.outcome shouldBe TaskOutcome.SUCCESS
+                    resolve("build/reports/tests/e2e").shouldBeADirectory()
+                }
             }
         }
     }
--- buildSrc/src/test/testing/kotlin/wolkenschloss/testing/Fixtures.kt
+++ buildSrc/src/test/testing/kotlin/wolkenschloss/testing/Fixtures.kt
@@ -3,27 +3,49 @@ package wolkenschloss.testing
 import java.io.File
 import java.nio.file.Paths
 
-class Fixtures(private val path: String) {
+class Fixtures(private val path: String) : AutoCloseable {
 
     fun clone(target: File): File {
         val fixtures = File(
-            System.getProperty("project.fixture.directory",
-                defaultFixturePath()
-            ))
+            System.getProperty(
+                "project.fixture.directory",
+                defaultFixturePath().absolutePath))
             .resolve(path)
 
         fixtures.copyRecursively(target)
         return target
     }
 
-    private fun defaultFixturePath() = Paths.get(System.getProperty("user.dir"), "fixtures").toAbsolutePath().toString()
+    private val temporaryDirectories = arrayListOf<File>()
+
+    private fun clone(): File {
+        val target = temporaryBuildDirectory().resolve(System.currentTimeMillis().toString())
+        temporaryDirectories.add(target)
+        return clone(target)
+    }
+
+    fun withClone(block: File.() -> Unit) {
+        val clone = clone()
+        block(clone)
+    }
+
+    private fun defaultFixturePath() = userDirectory().resolve("fixtures")
 
     private fun overlay(fixture: File) {
         val overlay = File(System.getProperty("project.fixture.directory")).resolve(path)
         overlay.copyRecursively(target = fixture, overwrite = false)
     }
 
-    private fun remove(fixture: File) {
+    fun useOverlay(directory: File, function: () -> Unit) {
+        Fixtures(path).overlay(directory)
+        try {
+            function()
+        } finally {
+            Fixtures(path).removeOverlay(directory)
+        }
+    }
+
+    private fun removeOverlay(fixture: File) {
         val fixtures = File(System.getProperty("project.fixture.directory"))
         val overlay = fixtures.resolve(path)
 
@@ -36,12 +58,24 @@ class Fixtures(private val path: String) {
         }
     }
 
-    fun useOverlay(fixture: File, function: () -> Unit) {
-        Fixtures(path).overlay(fixture)
-        try {
-            function()
-        } finally {
-            Fixtures(path).remove(fixture)
+    companion object {
+        fun userDirectory(): File = Paths.get(System.getProperty("user.dir")).toFile()
+
+        // It is not possible to use the temporary directories provided by
+        // Kotest or Java for cloned fixtures.
+        //
+        // The node_modules directory contains executable files that are
+        // required for the test. No files may be executed in proper temporary
+        // directories. This leads to an error in the test.
+//        val fixture = Fixtures.temporaryBuildDirectory()
+        fun temporaryBuildDirectory(): File = userDirectory().resolve(Paths.get("build", "tmp", "fixture").toFile())
+    }
+
+    override fun close() {
+        temporaryDirectories.reversed().forEach {directory ->
+            directory.walkBottomUp().forEach { file -> file.delete() }
         }
+
+        temporaryDirectories.clear()
     }
 }
