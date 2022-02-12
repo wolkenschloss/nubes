49fe393450e2da6e7c6e011177b46b05fa076aec
54v054r3n <17554782+haschi@users.noreply.github.com>
2022-02-12 17:11:04 +0100

    chore: custom IntelliJ scopes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,17 @@
 buildscript {
 
 }
+
+plugins {
+    idea
+}
+
+idea {
+    module {
+        excludeDirs.plusAssign(files(
+            ".git-hooks",
+            ".github",
+            ".run",
+            "gradle"))
+    }
+}
