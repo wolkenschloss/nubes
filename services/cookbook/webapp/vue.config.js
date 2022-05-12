a08ef346a13866d2f6ebdf6e5c1e770aa77820b4
54v054r3n <17554782+haschi@users.noreply.github.com>
2022-05-12 22:16:58 +0200

    build(cookbook): Adaptation of application icons for PWA
    - icon generator, icons and manifest added.
    
    Issue: #358

--- services/cookbook/webapp/vue.config.js
+++ services/cookbook/webapp/vue.config.js
@@ -1,6 +1,6 @@
 const options = {
-    key: "/home/matthias/localhost-key.pem",
-    cert: "/home/matthias/localhost.pem"
+    key: "localhost-key.pem",
+    cert: "localhost.pem"
 }
 const server = process.env.HTTPS === 'true' ? {type: 'https', options} : {type: 'http'}
 const webSocketURL = process.env.HTTPS === 'true' ? {protocol: 'wss'} : {protocol: 'ws'}
@@ -60,7 +60,51 @@ module.exports = {
         msTileColor: '#000000',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
-
+        manifestOptions: {
+            id: "/cookbook/",
+            start_url: "/cookbook/",
+            icons: [
+                {
+                    src: "/cookbook/images/icons/manifest-icon-512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                },
+                {
+                    src: "/cookbook/images/icons/manifest-icon-192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                },
+                {
+                    src: "/cookbook/images/icons/manifest-icon-512.maskable.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "maskable"
+                },
+                {
+                    src: "/cookbook/images/icons/manifest-icon-192.maskable.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                    purpose: "maskable"
+                },
+            ],
+            share_target: {
+                action: "/cookbook/share-target",
+                method: "GET",
+                params: {
+                    title: "title",
+                    text: "text",
+                    url: "url"
+                }
+            }
+        },
+        iconPaths: {
+            faviconSVG: 'favicon.svg',
+            favicon32: null,
+            favicon16:  null,
+            appleTouchIcon:  null,
+            maskIcon:  null,
+            msTileImage:  null,
+        },
         // configure the workbox plugin
         // workboxPluginMode: 'InjectManifest',
         workboxPluginMode: 'GenerateSW'
