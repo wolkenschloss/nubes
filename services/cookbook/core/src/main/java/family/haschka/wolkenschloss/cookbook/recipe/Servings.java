18c27ce25a5ef46a605f3039538bd1287e3d1fac
54v054r3n <17554782+haschi@users.noreply.github.com>
2022-05-31 10:13:58 +0200

    refactor(cookbook): move model into core project
    - Servings

--- services/cookbook/core/src/main/java/family/haschka/wolkenschloss/cookbook/recipe/Servings.java
+++ services/cookbook/core/src/main/java/family/haschka/wolkenschloss/cookbook/recipe/Servings.java
@@ -1,16 +1,12 @@
 package family.haschka.wolkenschloss.cookbook.recipe;
 
-import javax.json.bind.annotation.JsonbCreator;
-import javax.json.bind.annotation.JsonbProperty;
-
 import static java.lang.String.valueOf;
 
-public record Servings(@JsonbProperty("amount") int amount) {
+public record Servings(int amount) {
 
     public static final int MIN = 1;
     public static final int MAX = 100;
 
-    @JsonbCreator
     public Servings {
         if (amount < MIN || amount > MAX) {
             throw new IllegalArgumentException();
